49d21ccf3b9846e23a628ed73c10e4d9ab7a3283
mnzmlk <munazza.malik04@gmail.com>
2024-06-01 00:30:12 +0530

    Update oddnumbers.sh

--- oddnumbers.sh
+++ oddnumbers.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Create or clear the output file
-output_file="odd_numbers.txt"
+output_file="even_numbers.txt"
 > $output_file
 
 # Loop through numbers from 1 to 100
 for ((i=1; i<=100; i++))
 do
-    # Check if the number is odd
-    if ((i % 2! == 0))
+    # Check if the number is even
+    if ((i % 2 == 0))
     then
-        # Print the odd number to the output file
+        # Print the even number to the output file
         echo $i >> $output_file
     fi
 done
 
-echo "Odd numbers from 1 to 100 have been written to $output_file"
+echo "Even numbers from 1 to 100 have been written to $output_file"
